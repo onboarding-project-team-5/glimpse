63f972b8409cd68ef41ef60f089bdc7a7b679db8
yungi2383 <g830201@nate.com>
2023-08-09 17:15:10 -0700

    app.py 업데이트

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+# 식은 할당 대상이 될 수 없습니다? - 무슨 말인지 모르겠다
+# post 문법 찾아보고 수정하기
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 from pymongo import MongoClient
-from certifi import where
+import certifi
 
-DB_URL = '{DB URL 기입하시면 되요}'
-client = MongoClient(DB_URL, tlsCAFile=where())
+ca = certifi.where()
 
+client = MongoClient('mongodb+srv://yungiDB:dbsrlelql@cluster0.bpcqzfd.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
 
@@ -26,9 +29,36 @@ def page_profile_me():
     return render_template('profile.html')
 
 # 카드 등록 페이지
-@app.route('/card/registration')
-def page_card_registration():
-    return render_template('card_registeration.html')
+# @app.route('/card/registration')
+# def page_card_registration():
+#     return render_template('card_registeration.html')
+
+# 이미지 업로드 추가!!
+@app.route("/register", methods=["POST"])
+def register_post():
+    nickname_receive = request.form['nickname_give']
+    field_receive = request.form['field_give']
+    github_receive= request.form['github_give']
+    blog_receive = request.form['blog_give']
+    location_receive = request.form['lacation_give']
+    interest_receive = request.form['interest_give']
+    MBTI_receive = request.form['MBTI_give']
+    
+    register_list = list(db.register.find({}, {'_id': False}))
+    # 이미지 추가
+    doc = {
+        'nickname' = nickname_receive,
+        'field' = field_receive,
+        'github' = github_receive,
+        'blog' = blog_receive,
+        'location' = location_receive,
+        'interest' = interest_receive,
+        'MBTI' = MBTI_receive
+    }
+
+    db.cards.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
+
 
 # 404 오류 핸들러 - 잘못된 페이지로의 접근
 @app.errorhandler(404)
