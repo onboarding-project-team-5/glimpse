fe79a5259716140756f3a86f70a9c35f44114621
jieunpark23 <jenne7700@gmail.com>
2023-08-10 13:45:02 +0900

    데이터베이스 확인중

--- appprofile.py
+++ appprofile.py
@@ -79,3 +79,50 @@ def bucket_get():
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
 
+doc = {
+    'user_id' : 'mrhong',
+    'user_pw' : '1234',
+    'user_nickname': 'thief', # 닉네임
+    
+    'MBTI' : 'ENTJ', # mbti
+    'location': 'seoul', # 지역
+    'social_list': [{'facebook': 'fb.com/abc', 'github': 'github.com'} ],
+    'specialty' : 'spring', # 주특기 
+    'interest': ['tennis', 'swimming'], # 관심사
+    
+    'likes': 10, # 좋아요 
+    
+    'program' : '항해',
+    'course' : '웹개발 종합반', 
+    'cohort' : 16,
+    'team' : [5,10,11],
+    
+    'profile_image' : 'https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png',
+}
+
+db.user_list.insert_one(doc)
+
+doc = {
+    'user_id' : 'young',
+    'user_pw' : '1234',
+    'user_nickname': 'happy', # 닉네임
+    
+    'MBTI' : 'INTP', # mbti
+    'location': 'seoul', # 지역
+    'social_list': [{'facebook': 'fb.com/abc', 'github': 'github.com'} ],
+    'specialty' : 'Node.js', # 주특기 
+    'interest': ['tennis', 'swimming'], # 관심사
+    
+    'likes': 20, # 좋아요 
+    
+    'program' : '항해',
+    'course' : '웹개발 종합반', 
+    'cohort' : 16,
+    'team' : [2,9,11],
+    
+    'profile_image' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrcZnEnLy7Ie31zOFTPNd_C8GCFnYvy-Thcg&usqp=CAU',
+}
+
+db.user_list.insert_one(doc)
+
+print("load 완료")
